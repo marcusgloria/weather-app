07477972a56afc14670eb02991e54cf6e24049c4
marcusgloria <juniobarbosag@gmail.com>
2023-09-20 09:37:57 -0300

    version_1_ok

--- src/components/WeatherApp/WeatherApp.jsx
+++ src/components/WeatherApp/WeatherApp.jsx
@@ -1,8 +1,6 @@
 import React, { useState } from 'react'
 import './WeatherApp.css'
 
-import hot_background from "../assets/BACK-HOT-APP.png";
-import cold_background from "../assets/BACK-COLD-APP.png";
 import sun_icon from "../assets/SUN.png";
 import clear_icon from "../assets/clear-sky.png";
 import drizzle_icon from "../assets/drizzle.png";
@@ -12,35 +10,42 @@ import snow_icon from "../assets/snow.png";
 import search_icon from "../assets/search.png";
 
 const WeatherApp = () => {
+
+  const [cor_background, setCor] = useState("padrao");
  
   let api_key = "a59cae7790169635f887f04a20f7075b";
 
-  const [wicon, setWicon] = useState(sun_icon);
+  const [wicon, setWicon] = useState("null");
 
   console.log(api_key);
 
-
   const search = async () => {
-    const element = document.getElementsByClassName("cityInput");
 
+    const element = document.getElementsByClassName("cityInput");
     if(element[0].value===""){
       return 0;
     }
-
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&lang=pt&appid=${api_key}`;
-
-    console.log(url);
-
-    let response = await fetch(url);
-    let data = await response.json();
 
     const condition = document.getElementsByClassName("weather-condition");
     const temperature = document.getElementsByClassName("weather-temp");
     const location = document.getElementsByClassName("weather-location");
 
+    try{
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&lang=pt&appid=${api_key}`;
+    let response = await fetch(url);
+    let data = await response.json();
+
     condition[0].innerHTML = (data.weather[0].description).replace(/(^\w{1})|(\s+\w{1})/g, letra => letra.toUpperCase());
     temperature[0].innerHTML = Math.floor(data.main.temp)+" °C";
     location[0].innerHTML = `${data.name} - ${data.sys.country}`;
+
+
+    if(data.main.temp > 15){
+      setCor("red");
+    }
+    else{
+      setCor("blue");
+    }
 
     if(data.weather[0].icon==="01d" || data.weather[0].icon==="01n"){
       setWicon(sun_icon);
@@ -66,11 +71,17 @@ const WeatherApp = () => {
     else{
       setWicon(clear_icon);
     }
-
+    }catch(error){
+      setWicon("null");
+      location[0].innerHTML = "Digite uma cidade válida!";
+      condition[0].innerHTML = "";
+      temperature[0].innerHTML = "";
+      console.log(error);
+    }
   }
 
   return (
-    <div className='container'>
+    <div className='container' id={cor_background}>
       <div className='top-bar'>
         <input type="text" className="cityInput" placeholder='Pesquisar Cidade'/>
         <div className="search-icon" onClick={()=>{search()}}>
@@ -81,7 +92,7 @@ const WeatherApp = () => {
         <img src={wicon} width="250px" alt="" />
       </div>
       <div className="weather-temp"></div>
-      <div className="weather-location"></div>
+      <div className="weather-location">Insira o nome da cidade!</div>
       <div className="weather-condition"></div>
 
     </div>
